85ccd879dc33118c5536d3067c8410661a5848ed
Jongsu Liam Kim <appleparan@users.noreply.github.com>
2024-03-03 23:17:16 +0900

    Update Sphinx  (#9)
    * 240303: Update meeting log
    
    * Upgrade Sphinx
    
    * Add RTD badge
    
    * Fix Sphinx version

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,9 @@ html_theme = 'sphinx_book_theme'
 html_theme_options = {
     'repository_url': 'https://github.com/Lang-SIG/LangSIG',
     'use_download_button': False,
-    'single_page': False,
+}
+html_sidebars = {
+    "**": []
 }
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
