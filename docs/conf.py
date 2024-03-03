66876de7223a09aa0b565324e2a6d91edc1dd4fb
Jongsu Liam Kim <jongsukim8@gmail.com>
2024-03-03 23:41:20 +0900

    Downgrade Sphinx to 6.2.1
    * Remove navigation_with_keys deprecation warning
    * Remove pointless scrollbar on sidebar by some CSS Hack [link](https://github.com/executablebooks/sphinx-book-theme/issues/732#issuecomment-1802597436)

--- docs/conf.py
+++ docs/conf.py
@@ -56,10 +56,12 @@ html_theme = 'sphinx_book_theme'
 html_theme_options = {
     'repository_url': 'https://github.com/Lang-SIG/LangSIG',
     'use_download_button': False,
+    'navigation_with_keys': False
 }
-html_sidebars = {
-    "**": []
-}
+html_css_files = [
+    'css/custom.css',
+]
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
